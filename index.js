aa66cd1c16fb042e3cd73455470173fe6e8c8a5c
muhdhisham <muhdhisham034@gmail.com>
2021-01-07 21:37:33 +0530

    Added Particle-js

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
-var $input;
+// var $input;
 
-function onInputFocus(event) {
-  var $target = $(event.target);
-  var $parent = $target.parent();
-  $parent.addClass('input--filled');
-};
+// function onInputFocus(event) {
+//   var $target = $(event.target);
+//   var $parent = $target.parent();
+//   $parent.addClass('input--filled');
+// };
 
-function onInputBlur(event) {
-  var $target = $(event.target);
-  var $parent = $target.parent();
+// function onInputBlur(event) {
+//   var $target = $(event.target);
+//   var $parent = $target.parent();
 
-  if (event.target.value.trim() === '') {
-    $parent.removeClass('input--filled');
-  }
-};
+//   if (event.target.value.trim() === '') {
+//     $parent.removeClass('input--filled');
+//   }
+// };
 
-$(document).ready(function() {
-  $input = $('.input__field');
+// $(document).ready(function() {
+//   $input = $('.input__field');
   
-  // in case there is any value already
-  $input.each(function(){
-    if ($input.val().trim() !== '') {
-      var $parent = $input.parent();
-      $parent.addClass('input--filled');
-    }
-  });
+//   // in case there is any value already
+//   $input.each(function(){
+//     if ($input.val().trim() !== '') {
+//       var $parent = $input.parent();
+//       $parent.addClass('input--filled');
+//     }
+//   });
   
-  $input.on('focus', onInputFocus);
-  $input.on('blur', onInputBlur);
-});
+//   $input.on('focus', onInputFocus);
+//   $input.on('blur', onInputBlur);
+// });
